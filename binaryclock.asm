52d27e542ef4cef6917e6d5884abae55ab32913c
Dmitry Gorohov <dmitry.gorohov@voiapp.io>
2020-05-29 00:26:39 +0200

    RTC fixes

--- binaryclock.asm
+++ binaryclock.asm
@@ -4,7 +4,7 @@
 
 .def tmp 	= r16
 .def tmp_b 	= r17
-
+.def inp 	= r24
 
 .equ PCIE0 	  = 5
 .equ PCIE1 	  = 3
@@ -12,6 +12,11 @@
 
 .equ INT_PCMSK = PCMSK
 .equ INT_PCINT = PCINT0
+
+.equ BTN_PORT  = PORTB
+.equ BTN_PIN   = PINB
+.equ BTN_DIR   = DDRB
+.equ BTN_BIT   = PB0
 
 .dseg
 .org SRAM_START
@@ -21,7 +26,7 @@ current_minute:	.byte 1
 current_second:	.byte 1
 current_column:	.byte 1
 num_seconds: 	.byte 1
-
+current_mode:	.byte 2
 
 .cseg
 .org 0x0000
@@ -49,42 +54,50 @@ reti 		; PCINT1
 rjmp update_current_time  		; PCINT2
 
 init:
-	ldi tmp, LOW(RAMEND)
+	ldi zl, 30 	; clear registers
+	clr zh
+	dec zl
+	st z, zh
+	brne PC-2
+ramflush:
+	ldi zl, low(SRAM_START)
+	ldi zh, high(SRAM_START)
+	clr tmp
+loop_ramflush:
+	st z+, tmp
+	cpi zl, low(RAMEND + 1)
+	brne loop_ramflush
+	cpi zh, high(RAMEND + 1)
+	brne loop_ramflush
+init_stack:
+	ldi tmp, low(RAMEND)
 	out SPL, tmp
-
-	ldi tmp, 1 << 3
-	sts current_column, tmp
-
-	ldi tmp, 0x00
-	sts current_second, tmp
-	sts current_minute, tmp
-	sts current_hour, tmp
 
 power_reduction:
 	in	tmp, PRR
 	ori	tmp, (1 << PRUSI) | (1 << PRUSART) | (1 << PRTIM1)
 	out	PRR, tmp
-
-	cbi ACSR, ACIE 			; Analog Comparator Interrupt Disable
-	cbi ACSR, ACIC 			; Analog Comparator Input Capture Disable
-	sbi ACSR, ACD 			; Analog Comparator Disable
-
-	ldi tmp, 0x00 			; Disable all analog inputs
-	out DIDR, tmp
-
-	ldi tmp, 0x00			; Disable USICR
+; Analog Comparator Disable
+	cbi ACSR, ACIE
+	cbi ACSR, ACIC
+	sbi ACSR, ACD
+; Disable all analog inputs
+	sbi DIDR, AIN0D
+	sbi DIDR, AIN1D
+; Disable USICR
+	ldi tmp, 0x00
 	out USICR, tmp
-
-	ldi tmp, 0b00000110 	; Disable UCSRB interrupts
+; Disable UCSRB interrupts
+	ldi tmp, 0b00000110
 	out UCSRB, tmp
 
 ; Disable watchdog
-	in tmp, MCUSR
-	ori tmp, (0 << WDRF)	; Disable watchdog
-	out MCUSR, tmp
+	wdr
+	ldi	tmp, (0 << WDRF)	; Clear WDRF in MCUSR
+	out	MCUSR, tmp
 
 	in tmp, WDTCR
-	ori tmp, 0x18 			; (1 << WDCE) | (1 << WDE)
+	ori tmp, (1 << WDCE) | (1 << WDE)
 	out WDTCR, tmp
 
 	ldi tmp, (0 << WDE)
@@ -101,8 +114,10 @@ main:
 other_init:
 	rcall hc595_init
 	rcall spi_init
-	rcall rtc_init
 .ifdef TURN_OFF_PWM
+	in tmp, PRR
+	ori tmp, (1 << PRTIM0) 		; Disable Timer
+	out PRR, tmp
 	rjmp start
 .endif
 
@@ -116,100 +131,72 @@ pwm_setup0:
 	ldi tmp, BRIGHTNESS 		; BRIGHTNESS: 0 - full, 255 - off
 	out OCR0A, tmp
 
-	in r24, TIMSK
-	andi r24, ~(1 << OCIE0A) & ~(1 << OCIE0B)
-	out TIMSK, r24
+	in tmp, TIMSK
+	cbr tmp, (1 << OCIE0A) | (1 << OCIE0B)
+	out TIMSK, tmp
 
 start:
-	in tmp, DDRB
-	ori tmp, 1 << PB0
-	out DDRB, tmp
+	in tmp, BTN_DIR
+	ori tmp, (1 << BTN_BIT)
+	out BTN_DIR, tmp
 
+	rcall timer1s
+	rcall rtc_init
 	sei
 
 loop:
-	ldi r22, 0
-	sts num_seconds, r22
 	rcall hc595_reset
+	clr tmp
+	sts num_seconds, tmp
+	sts current_mode, tmp
+	sts current_mode + 1, tmp
+	ldi tmp, 1
+	sts current_column, tmp
+
 	rcall rtc_disable_int
-	rcall sleep_mode 			; sleep
+	rcall sleep_mode 					; sleep
 	rcall rtc_read_time
 	rcall rtc_enable_int
 
 loop_continue:
-	lds r22, num_seconds
-	cpi r22, 20
-	brge loop
-
+	lds tmp, num_seconds
+	cpi tmp, 20 						; ~10 sec or 1/64ms two interrupts every second
+	brge loop 							; stop loop if 10sec passed
 update_hc595:
-	clc
 	lds tmp, current_column
-	lsr tmp 					; current_column >> 1
-	brcc compa_set_current
-	ldi tmp, 1 << 3				; if so, current_column = 1 << 3
-compa_set_current:
-	sts current_column, tmp 	; set it with a new value
+	lsl tmp 							; current_column << 1
+	andi tmp, 0x0F 						; check if bit have left low nibble's boundaries
+	brne PC+2 							; if not skip next
+	ldi tmp, 1 							; if so, current_column = 1
+	sts current_column, tmp 			; set it with a new value
 
-first_column:
-	lds r24, current_second 	; load current_minute value
+	mov tmp_b, tmp
+	andi tmp_b, (1 << 3) | (1 << 2) 	; isolate 3 and 2 bits
+	brne PC+4							; PC+4 if not set (neq. 0)
+	lds r24, current_minute
+	rjmp PC+2
+	lds r24, current_second
 
-	ldi tmp_b, 1 << 3
-	cpse tmp, tmp_b 			; if not first column
-	rjmp second_column 			; go and check next column
-
-	swap r24					; (current_minute >> 4) & 0x0F
+	mov tmp_b, tmp
+	andi tmp_b, (1 << 3) | (1 << 1) 	; check if 3 and 1 bits are set, e.g. high bits
+	brne PC+2 							;
+	swap r24
 	andi r24, 0x0F
 
-	rjmp update
-
-second_column:
-	ldi tmp_b, 1 << 2
-	cpse tmp, tmp_b 			; if not second column
-	rjmp third_column 			; go and check next column
-	
-	andi r24, 0x0F
-
-	rjmp update
-
-third_column:
-	lds r24, current_minute 		; load current_second value
-
-	ldi tmp_b, 1 << 1
-	cpse tmp, tmp_b 			; if not second column
-	rjmp fourth_column 			; go and check next column
-
-	swap r24					; (current_second >> 4) & 0x0F
-	andi r24, 0x0F
-
-	rjmp update
-
-fourth_column:
-	ldi tmp_b, 1 << 0
-	cpse tmp, tmp_b 			; if not second column
-	rjmp update 				; go and check next column
-	
-	andi r24, 0x0F
-
-update:
 	com tmp
-	andi tmp, 0xF
+	andi tmp, 0xF 						; invert column. 1 - means off, 0 - on
 
 	swap r24
 	or r24, tmp
-
-	; r24 - row
-	; tmp - column
 	; RRRRCCCC
 	rcall hc595_send_byte
-
 	rjmp loop_continue
 
 ; +++++++++++++++++++++++++++
 sleep_mode:
-	in tmp, PRR
-	ori tmp, (1 << PRTIM0) 		; Disable Timer
-	out PRR, tmp
-
+.ifndef TURN_OFF_PWM
+	sbi PRR, PRTIM0 			; Disable Timer
+.endif
 	in tmp, INT_PCMSK
 	ori tmp, (1 << INT_PCINT)	; PCINT enable
 	out INT_PCMSK, tmp
@@ -229,43 +216,43 @@ sleep_mode:
 	ori tmp, (1 << PCINT12)		; PCINT12 RTC int enable again
 	out PCMSK2, tmp
 
-	in tmp, PRR
-	andi tmp, ~(1 << PRTIM0)	; Enable Timer
-	out PRR, tmp
+.ifndef TURN_OFF_PWM
+	cbi PRR, PRTIM0				; Enable Timer
+.endif
 
 	in tmp, INT_PCMSK
 	andi tmp, ~(1 << INT_PCINT) ; PCINT disable
 	out INT_PCMSK, tmp
 
 	in tmp, MCUCR
-	andi tmp, ~(1 << SE) 	; Sleep disable
+	andi tmp, ~(1 << SE) 		; Sleep disable
 	out MCUCR, tmp
 
 	sei
-
 	ret
 
 update_current_time:
-	push r25 				; keep r25
-	push r24 				; keep r24
+	push r25 					; keep r25
+	push r24 					; keep r24
 
-	cli 					; disable interrupts globally
+	cli 						; disable interrupts globally
 
-	in r24, SREG			;
-	push r24				; keep SREG
+	in r24, SREG				;
+	push r24					; keep SREG
 
 	rcall rtc_read_time
 
-	lds r25, num_seconds
+	lds r25, num_seconds		; go here
 	inc r25
 	sts num_seconds, r25
 
-	pop r24					;
-	out SREG, r24			; restore SREG
-	pop r24					; restore r24
-	pop r25					; restore r25
+	pop r24						;
+	out SREG, r24				; restore SREG
+	pop r24						; restore r24
+	pop r25						; restore r25
 	reti
 
 .include "rtc.inc"
 .include "spi.inc"
 .include "hc595.inc"
+.include "utils.inc"
